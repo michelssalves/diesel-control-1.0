0160ce975ef7261b0bb26afc746d91a24be8bc46
unknown <michelssalves@gmail.com>
2022-06-18 00:03:37 -0300

    17-06 00:04

--- logar_controle_de_abastecimento.php
+++ logar_controle_de_abastecimento.php
@@ -3,8 +3,15 @@ session_start();
 require 'conexao01.php';
 
 if (isset($_POST['usuario']) && !empty($_POST['usuario']) && isset($_POST['senha']) && !empty($_POST['senha'])) {
+	
 	$usuario = addslashes($_POST['usuario']);
 	$senha = addslashes($_POST['senha']);
+
+	$token = md5(time() . rand(0, 9999) . time());
+	$sql = $pdo->prepare("UPDATE funcionarios SET token = :token WHERE usuario = :usuario");
+	$sql->bindValue(':token', ($token));
+	$sql->bindValue(':usuario', $usuario);
+	$sql->execute();
 
 	$sql = "SELECT * FROM funcionarios WHERE usuario = :usuario AND senha = :senha";
 	$sql = $pdo->prepare($sql);
@@ -17,42 +24,44 @@ if (isset($_POST['usuario']) && !empty($_POST['usuario']) && isset($_POST['senha
 	if ($sql->rowCount() > 0) {
 
 		while ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
-			$id_funcionario['id_funcionario'] = $row['id_funcionario'];
-			$tipo_acesso['tipo_acesso'] = $row['tipo_acesso'];
-			$nome['nome'] = $row['nome'];
+			$id_funcionario = $row['id_funcionario'];
+			$tipo_acesso = $row['tipo_acesso'];
+			$nome = $row['nome'];
+			$nome = $row['usuario'];
+			$matricula = $row['matricula'];
+			$token = $row['token'];
 		}
-		$token = md5(time() . rand(0, 9999) . time());
-		$sql = $pdo->prepare("UPDATE funcionarios SET token = :token WHERE usuario = :usuario");
-		$sql->bindValue(':token', ($token));
-		$sql->bindValue(':usuario', $usuario);
-		$sql->execute();
+
 		if ($tipo_acesso['tipo_acesso'] == 1) {
+			$_SESSION['id_funcionario'] = $id_funcionario['id_funcionario'];
 			$_SESSION['tipo_acesso'] = $tipo_acesso['tipo_acesso'];
-			$_SESSION['token'] = $token;
+			$_SESSION['nome'] = $nome;
 			$_SESSION['usuario'] = $usuario;
 			$_SESSION['matricula'] = $matricula;
-			$_SESSION['id_funcionario'] = $id_funcionario['id_funcionario'];
+			$_SESSION['token'] = $token;
+			
 			$_SESSION['nome'] = $nome['nome'];
 			header('Location: ../registros/registroDeAbastecimento.php');
 			exit();
 		} elseif ($tipo_acesso['tipo_acesso'] == 2) {
+			$_SESSION['id_funcionario'] = $id_funcionario['id_funcionario'];
 			$_SESSION['tipo_acesso'] = $tipo_acesso['tipo_acesso'];
-			$_SESSION['token'] = $token;
+			$_SESSION['nome'] = $nome;
 			$_SESSION['usuario'] = $usuario;
 			$_SESSION['matricula'] = $matricula;
-			$_SESSION['id_funcionario'] = $id_funcionario['id_funcionario'];
-			$_SESSION['nome'] = $nome['nome'];
+			$_SESSION['token'] = $token;
+
 			header('Location: ../relatorios/relatoriosHtml.php');
 			exit();
 		} else {
 			$_SESSION['msg'] = "<div class='alert alert-danger'></div>";
-			header('Location:login_controle_de_abastecimento.php');
+			header('Location:login-diesel-control-1.0');
 			exit();
 		}
 	}
 	else {
 			$_SESSION['msg'] = "<div class='alert alert-danger'></div>";
-			header('Location:login_controle_de_abastecimento.php');
+			header('Location:login-diesel-control-1.0');
 			exit();
 		}
 }
