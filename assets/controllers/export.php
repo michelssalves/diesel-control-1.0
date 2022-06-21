8c1c6f743478de7fe11e67dbde0aca612c836658
Michel <michelssalves@gmail.com>
2022-06-21 17:09:37 -0300

    2022-06-21 16:17

--- assets/controllers/export.php
+++ assets/controllers/export.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-include '../assets/controllers/config.php';
 $id_funcionario = $_SESSION['id_funcionario'];
 $tipo_acesso = $_SESSION['tipo_acesso'] ;
 $nome = $_SESSION['nome'];
@@ -9,6 +8,8 @@ $matricula = $_SESSION['matricula'];
 $token = $_SESSION['token'];
 include '../assets/controllers/checkAcess.php';
 
+$acao = $_REQUEST['acao'];
+if($acao == 'exportar'){
 $data_abastecimento = date_create($_POST['data_abastecimento']);
 $data_abastecimento1 = date_format($data_abastecimento, 'Y-m-d H:i:s');
 //$numero_equipamento = $_POST['numero_equipamento'];
@@ -100,6 +101,6 @@ header("Content-Disposition: attachment; filename=\"$fileName\"");
  
 // Render excel data 
 echo $excelData; 
- 
+}
 
 ?>
