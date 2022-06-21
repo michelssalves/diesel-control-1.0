18e9640bd89e8d9b3a6125844c8a40798dffcbca
Michel <michelssalves@gmail.com>
2022-06-21 16:54:59 -0300

    2022-06-21 16:17

--- relatorios/relatoriosHtml.php
+++ relatorios/relatoriosHtml.php
@@ -17,7 +17,7 @@ include '../assets/controllers/checkAcess.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="diesel-control-1.0/assets/css/style.css">
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="diesel-control-1.0/assets/js/filtroTabela.js"></script>
 </head>
 <body>
