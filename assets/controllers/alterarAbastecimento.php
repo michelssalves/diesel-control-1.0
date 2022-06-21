9cb39786fd658a483462d11e4c46aba58a69fc38
Michel <michelssalves@gmail.com>
2022-06-21 13:01:16 -0300

    2022-06-21 13:01

---
+++ assets/controllers/alterarAbastecimento.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+include '../assets/controllers/config.php';
+$id_funcionario = $_SESSION['id_funcionario'];
+$tipo_acesso = $_SESSION['tipo_acesso'] ;
+$nome = $_SESSION['nome'];
+$usuario = $_SESSION['usuario'];
+$matricula = $_SESSION['matricula'];
+$token = $_SESSION['token'];
+include '../assets/controllers/checkAcess.php';
+
+$combustivel = $_POST['combustivel'];
+$odometroinicial = $_POST['odometroinicial'];
+$odometrofinal = $_POST['odometrofinal'];
+$litros = $_POST['litros'];
+$ultimokm = $_POST['ultimokm'];
+$km = $_POST['km'];
+$hr = $_POST['hr'];
+$frentista = $_POST['frentista'];
+$litros_od = $_POST['litros_od'];
+$diferencakm = $_POST['diferencakm'];
+$media = $_POST['media'];
+$id_abastecimento = intval($_POST['id_abastecimento']);
+$id_veiculo = $_POST['id_veiculo'];
+
+if($id_veiculo){
+        $sql = $pdo->prepare("UPDATE abastecimentos SET
+        id_veiculo = :id_veiculo,
+        odometroinicial = :odometroinicial,
+        combustivel = :combustivel,
+        ultimokm = :ultimokm,
+        km = :km,
+        diferencakm = :diferencakm,
+        hr = :hr,
+        frentista = :frentista,
+        odometrofinal = :odometrofinal,
+        litros = :litros,
+        litros_od = :litros_od,
+        media = :media
+        WHERE id_abastecimento = :id_abastecimento");
+        $sql->bindValue(':id_veiculo', $id_veiculo);
+        $sql->bindValue(':odometroinicial', $odometroinicial);
+        $sql->bindValue(':combustivel', $combustivel);
+        $sql->bindValue(':ultimokm', $ultimokm);
+        $sql->bindValue(':km', $km);
+        $sql->bindValue(':diferencakm', $diferencakm);
+        $sql->bindValue(':hr', $hr);
+        $sql->bindValue(':frentista', $frentista);
+        $sql->bindValue(':odometrofinal', $odometrofinal);
+        $sql->bindValue(':litros', $litros);
+        $sql->bindValue(':litros_od', $litros_od);
+        $sql->bindValue(':media', $media);
+        $sql->bindValue(':id_abastecimento', $id_abastecimento);
+        $sql->execute();
+       header("Location: controle-almoxarifado");
+        exit;
+    } else {
+        header("Location: alterar-abastecimento-v1");
+     exit;
+    }
+
+ 
---
+++ assets/controllers/formController.php
@@ -0,0 +1,119 @@
+<?php
+session_start();
+include '../assets/controllers/config.php';
+$id_funcionario = $_SESSION['id_funcionario'];
+$tipo_acesso = $_SESSION['tipo_acesso'] ;
+$nome = $_SESSION['nome'];
+$usuario = $_SESSION['usuario'];
+$matricula = $_SESSION['matricula'];
+$token = $_SESSION['token'];
+include '../assets/controllers/checkAcess.php';
+
+$id_veiculo = $_POST['id_veiculo'];
+$numero_equipamento = $_POST['numero_equipamento'];
+$placa = $_POST['placa'];
+$metodo = $_POST['metodo'];
+$bomba = $_POST['bomba'];
+$litros_od = $_POST['litros_od'];
+$media = $_POST['media'];
+$prefixo = $_POST['prefixo'];
+$ultimokm = $_POST['ultimokm']; 
+$km = $_POST['km']; 
+$diferencakm = $_POST['diferencakm'];
+$ultimohr = $_POST['ultimohr']; 
+$hr = $_POST['hr']; 
+$diferencahr = $_POST['diferencahr']; 
+$combustivel = $_POST['combustivel']; 
+$odometroinicial = $_POST['odometroinicial']; 
+$odometrofinal = $_POST['odometrofinal']; 
+$litros = $_POST['litros']; 
+$frentista = $_POST['frentista'];
+$datahora1 = new DateTime("now", new DateTimeZone('America/Sao_Paulo'));
+$datahora = $datahora1->format("Y-m-d H:i:s");	
+$datahora2 = $datahora1->format("d-m-Y H:i:s");	
+$dataabastecimento2 = $datahora1->format("Y-m-d");
+$diferenca = 3000;
+$abastecimento = 400;
+
+if($litros_od < $abastecimento){
+	if($id_veiculo){
+		if($id_veiculo){
+
+		$sql = $pdo->prepare("INSERT INTO abastecimentos (
+		id_veiculo,	 
+		combustivel, 
+		bomba,
+		odometroinicial,
+		ultimokm,	
+		km,
+		diferencakm,
+		ultimohr,
+		hr,
+		diferencahr,
+		frentista,	
+		odometrofinal, 
+		litros,
+		litros_od,
+		media,
+		data_abastecimento,
+		dataabastecimento2) 
+		VALUES (	
+		:id_veiculo, 
+		:combustivel, 
+		:bomba, 
+		:odometroinicial,
+		:ultimokm,
+		:km, 
+		:diferencakm,
+	    :ultimohr,
+		:hr,
+		:diferencahr,
+		:frentista,
+		:odometrofinal,
+		:litros,
+		:litros_od,
+		:media,
+		:datahora,
+		:dataabastecimento2)");
+		$sql->bindValue(':id_veiculo', $id_veiculo);
+		$sql->bindValue(':combustivel', $combustivel);
+		$sql->bindValue(':bomba', $bomba);
+		$sql->bindValue(':odometroinicial', $odometroinicial);
+		$sql->bindValue(':ultimokm', $ultimokm);
+		$sql->bindValue(':km', $km);
+		$sql->bindValue(':diferencakm', $diferencakm);
+		$sql->bindValue(':ultimohr', $ultimohr);
+		$sql->bindValue(':hr', $hr);
+		$sql->bindValue(':diferencahr', $diferencahr);
+		$sql->bindValue(':frentista', $frentista);
+		$sql->bindValue(':odometrofinal', $odometrofinal);
+		$sql->bindValue(':litros', $litros);
+		$sql->bindValue(':litros_od', $litros_od);
+		$sql->bindValue(':media', $media);
+		$sql->bindValue(':datahora', $datahora);
+		$sql->bindValue(':dataabastecimento2', $dataabastecimento2);
+		$sql->execute();
+		$_SESSION['msg'] = "<div class='alert alert-sucess'>Cadastro Realizado com Sucesso!</div>";
+		header("Location: abastecimento-da-frota");
+		exit;
+    	header("Location: abastecimento-da-frota");
+		}else{
+			$_SESSION['msg'] = "<div class='alert alert-danger'>Não foi encontrado esse veiculo!</div>";
+			header("Location: abastecimento-da-frota");
+			exit;
+		}
+
+}else{
+	$_SESSION['msg'] = "<div class='alert alert-danger'>O Km Informado está incorreto!</div>";
+	header("Location: abastecimento-da-frota");
+	exit;
+}
+}else{
+	$_SESSION['msg'] = "<div class='alert alert-danger'>O Odometro informado está incorreto!</div>";
+	header("Location: abastecimento-da-frota");
+	exit;
+}	
+
+
+  
+
